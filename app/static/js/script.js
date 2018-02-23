e228a2cace8a4cdd359f3650894f3b3a9e83a7c7
Jamie Jansen <32266479+jajan20@users.noreply.github.com>
2018-02-23 14:39:58 +0100

    global script for starting modules

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,36 +1,5 @@
-// Create local scope
-(function() {
-    // Initialize application.
-    var app = {
-        // Method
-        init: function() {
-            routes.init()
-        }
-    }
+import app from './app.js'
 
-    // Handle routes & state
-    var routes = {
-        init: function(event) {
-            window.addEventListener("hashchange",function(){
-                // console.log('hash has changed!')
-                var route = window.location.hash.substring(1)
-                // console.log(route)
-                sections.toggle(route)
-            })
-        }
-    }
-
-    // Render / toggle sections
-    var sections = {
-        toggle: function(currentRoute) {
-            var sections = document.getElementsByClassName('section')
-            
-            for (let s of sections) {
-                s.id === currentRoute ? s.classList.add('active') : s.classList.remove('active')
-            }
-        }
-    }
-
-    // Start application
-    app.init()
+(function(){
+	app.init()
 })()
